bd332dcd2e652e8699d8dd05c5989c2ecefacfca
Amanda <amandaben21@aol.com>
2021-05-22 19:25:54 -0400

    Done

--- db/migrate/20210522222026_create_sightings.rb
+++ db/migrate/20210522222026_create_sightings.rb
@@ -3,7 +3,7 @@ class CreateSightings < ActiveRecord::Migration[5.2]
     create_table :sightings do |t|
       t.references :bird, foreign_key: true
       t.references :location, foreign_key: true
-
+      
       t.timestamps
     end
   end
